9cbbbd2c3fa48b0f3fe648b2075effbda4600a3a
Nikita Simakov <everm1nd.mail@gmail.com>
2013-09-16 22:45:49 +0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPushstate::Application.config.secret_token = '389bfd616413294080e6598d177605049a24acdb5f0a28a5f4cf73847df62713ac8f5e37aa6d6a54ad4df02b824987f13def87c2ff99ae23bc789501005d2508'
